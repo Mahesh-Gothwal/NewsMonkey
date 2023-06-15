3d9b2dbe022e5b15337637831406c60fe7db368e
Mahesh Gothwal <maheshgothwal1234@gmail.com>
2023-06-15 11:31:58 +0530

    Setting State in Class based components

--- src/components/News.js
+++ src/components/News.js
@@ -8,7 +8,7 @@ export class News extends Component {
         <h2>NewsMonkey - Top Headlines</h2>
         <div className="row">
           <div className="col-md-4">
-        <NewsItem title="myTitle" description="mydesc"/>
+        <NewsItem title="myTitle" description="mydesc" imageUrl="https://media.cnn.com/api/v1/images/stellar/prod/230613231941-04-knights-stanley-cup-061323.jpg?c=16x9&q=w_800,c_fill" newsUrl = "todo"/>
           </div>
           <div className="col-md-4">
         <NewsItem title="myTitle" description="mydesc"/>
