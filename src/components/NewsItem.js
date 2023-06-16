fbe2a2c0f0a54fbc618f167f7ee1b0b9ad924a3a
Mahesh Gothwal <maheshgothwal1234@gmail.com>
2023-06-16 13:06:39 +0530

    Adding Categories & ProtoTypes

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,7 +5,7 @@ export class NewsItem extends Component {
     let {title, description, imageUrl, newsUrl} = this.props;
     return (
       <div className="my-3">
-        <div className="card" style={{width: "18rem"}}>
+        <div className="card">
           <img src={!imageUrl?"https://img.etimg.com/thumb/msid-100973137,width-1070,height-580,imgsize-91161,overlay-etmarkets/photo.jpg":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
