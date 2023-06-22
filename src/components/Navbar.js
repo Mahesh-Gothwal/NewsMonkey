41a2a32094b700ed433b150127bab85bd8a7e901
Mahesh Gothwal <maheshgothwal1234@gmail.com>
2023-06-22 21:36:09 +0530

    Changing Class to function based components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-export class Navbar extends Component {
-  render() {
+const Navbar = ()=>{
     return (
         <div>
         <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
@@ -27,7 +26,6 @@ export class Navbar extends Component {
       </nav>
       </div>
     )
-  }
 }
 
 export default Navbar
